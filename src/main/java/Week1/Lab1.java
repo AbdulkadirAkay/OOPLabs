f1124fc0544827f3a9298fbf42adb48299dc70ee
AbdulkadirAkay <116177045+AbdulkadirAkay@users.noreply.github.com>
2024-10-09 16:12:42 +0200

    Commited

---
+++ src/main/java/Week1/Lab1.java
@@ -0,0 +1,52 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // 1.Create a program that prints your name to the screen. The program output should resemble the following:
+        /*String name = "Abdulkadir";
+        System.out.println("My name is " + name);*/
+
+        // 2.Create a program that prints out the following:
+        /*System.out.println("Hello World!");
+        System.out.println("(And all)");*/
+
+        // 3.Create a program that counts and displays how many seconds there are in a year.
+        // You can assume that a year consists of 365 days (therefore the year is not a leap year).
+        //System.out.println("There are " + 365 * 24 * 60 * 60 + " seconds in 1 year");
+
+        // 4.Create a program that asks the user for two integers and then prints their sum.
+        /*System.out.println("Enter first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum is " + sum);*/
+
+        // 5.Create a program that asks the user for a number and tells if the number is positive (i.e. greater than zero).
+        /*System.out.println("Enter a number");
+        int num = scanner.nextInt();
+        if (num > 0) {
+            System.out.println("Its positive");
+        } else {
+            System.out.println("Its negative");
+        }*/
+
+        // 6.Create a program that asks the user for two numbers and prints the greater of those two.
+        // The program should also handle the case in which the two numbers are equal.
+        /*System.out.println("Enter first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number");
+        int num2 = scanner.nextInt();
+        if (num1 > num2) {
+            System.out.println("Number 1 is greater than Number 2");
+        } else if (num2 > num1) {
+            System.out.println("Number 2 is greather than Number 1");
+        } else {
+            System.out.println("Numbers are equal");
+        }*/
+
+    }
+}
